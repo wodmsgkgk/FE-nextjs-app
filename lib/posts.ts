d334dea795ca918e81b445be7de359ea6ac25378
wodmsgkgk <wodmsgkgk@naver.com>
2022-09-05 17:15:40 +0900

    posts.ts update

---
+++ lib/posts.ts
@@ -0,0 +1,32 @@
+import fs from 'fs'
+import path from 'path'
+import matter from 'gray-matter'
+
+const postDirectory = path.join(process.cwd(), 'posts')
+console.log('')
+
+export function getSortedPostData() {
+    const fileNames = fs.readdirSync(postDirectory)
+
+    const allPostsData = fileNames.map(fileName => {
+        const id = fileName.replace(/\.md$/, '')
+
+        const fullPath = path.join(postDirectory, fileName)
+        const fileContents = fs.readFileSync(fullPath, 'utf8')
+
+        const matterResult = matter(fileContents)
+
+        return(
+            id,
+            ...matterResult.data as { date:string; title: string}
+        )
+    })
+
+    return allPostsData.sort{(a, b) => {
+        if(a.date < b.date) {
+            return 1
+        } else {
+            return -1
+        }
+    }}
+}
